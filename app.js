4c359327aae8fd8375b8a484d1b827036104c52d
= <=>
2024-08-16 19:20:02 +0200

    Hello Word with Vue

---
+++ app.js
@@ -0,0 +1,27 @@
+const { createApp, ref } = Vue;
+
+const app = createApp({
+    template: `
+        <h1>{{ message }}</h1>
+        <p>- {{author}}</p>
+    `,
+    setup() {
+        // Con ref() creamos una varianle reactiva.
+        const message = ref("I'm Batman");
+        const author = ref('Bruce Wayne');
+        
+        setTimeout(() =>{
+            // Usando .value en la variable reactiva, podemos cambiar el valor.
+            message.value = 'Hola, Soy Goku';
+            author.value = 'Goku';
+            
+        }, 1500)
+
+        return {
+            message,
+            author,
+        };
+    },
+});
+
+app.mount('#myApp');
