a98b9274f92ae49ed98efefc99fce460fc7bf869
= <=>
2024-08-17 08:56:41 +0200

    Separate HTML and Events

--- intro/app.js
+++ intro/app.js
@@ -1,25 +1,32 @@
 const { createApp, ref } = Vue;
 
 const app = createApp({
-    template: `
-        <h1>{{ message }}</h1>
-        <p>- {{author}}</p>
-    `,
+    // template: `
+    //     <h1>{{ message }}</h1>
+    //     <p>- {{author}}</p>
+    // `,
     setup() {
         // Con ref() creamos una varianle reactiva.
         const message = ref("I'm Batman");
         const author = ref('Bruce Wayne');
-        
-        setTimeout(() =>{
-            // Usando .value en la variable reactiva, podemos cambiar el valor.
+
+        const changeMessage = () => {
             message.value = 'Hola, Soy Goku';
             author.value = 'Goku';
+        }
+        
+        // setTimeout(() =>{
+        //     // Usando .value en la variable reactiva, podemos cambiar el valor.
+        //     message.value = 'Hola, Soy Goku';
+        //     author.value = 'Goku';
             
-        }, 1500)
+        // }, 1500)
 
         return {
             message,
             author,
+
+            changeMessage,
         };
     },
 });
